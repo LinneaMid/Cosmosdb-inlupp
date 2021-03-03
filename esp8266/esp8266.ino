ac0b173d9bde82c320e050bf7cb5585d0cab13b1
Linnea Miderkvist <Linnea.miderkvist@gmail.com>
2021-03-03 23:17:41 +0100

    all added

---
+++ esp8266/esp8266.ino
@@ -0,0 +1,71 @@
+#include <ESP8266WiFi.h>
+#include <AzureIoTHub.h>
+#include <AzureIoTProtocol_MQTT.h>
+#include <AzureIoTUtility.h>
+#include <DHT.h>
+#include <ArduinoJson.h>
+
+#define DHT_PIN 2
+#define DHT_TYPE DHT11
+
+char* ssid = "Mi_Guest";
+char* pass = "Mider@7719950";
+char* conn = "HostName=iot20-lin-iothub.azure-devices.net;DeviceId=ESP8266;SharedAccessKey=9elwtEoJcmrNjLsifwSSCKJ1G3wk26f+aHFVaUeEFMU=";
+
+bool messagePending = false;
+float prevTemp = 0;
+int diff = 1;
+time_t epochTime;
+
+DHT dht(DHT_PIN, DHT_TYPE);
+IOTHUB_CLIENT_LL_HANDLE deviceClient;
+
+void setup() {
+  initSerial();
+  initWifi();
+  initEpochTime();
+  initDHT();
+  initDevice();
+}
+
+void loop() {
+  float temperature = dht.readTemperature();
+  float humidity = dht.readHumidity();
+
+  float currentTemp = temperature;
+
+  if (!messagePending) {
+
+    if ((currentTemp >= (prevTemp + diff)) || (currentTemp <= (prevTemp - diff))) {
+      prevTemp = currentTemp;
+
+      epochTime = time(NULL);
+      Serial.printf("Current Time: %lu. ", epochTime);
+
+      Serial.print("Temperature: ");
+      Serial.print(temperature);
+      Serial.print(", Humidity: ");
+      Serial.println(humidity);
+
+      delay(500);
+
+      if (!(std::isnan(temperature)) && !(std::isnan(humidity)))  {
+        char payload[256];
+
+        StaticJsonBuffer<sizeof(payload)> buf;
+        JsonObject &root = buf.createObject();
+        root["deviceId"] = "esp8266";
+        root["ts"] = epochTime;
+        root["temp"] = temperature;
+        root["hum"] = humidity;
+        root.printTo(payload, sizeof(payload));
+
+        sendMessage(payload);
+      }
+    }
+  }
+
+  IoTHubClient_LL_DoWork(deviceClient);
+  delay(10);
+
+}
---
+++ website/script.js
@@ -0,0 +1,14 @@
+let tabledata = document.getElementById('tabledata')
+
+fetch("https://iot20-lin-fa.azurewebsites.net/api/GetAllFromCosmosDb")
+.then(res => res.json())
+.then(data => {
+    console.log(data)
+    for(let row of data) {
+
+        var unixTimestamp = row.ts;
+        var date = new Date(unixTimestamp*1000);
+
+        tabledata.innerHTML += `<tr><td>4${row.id}</td><td>${row.deviceId}</td><td>${date}</td><td>${row.temp}</td><td>${row.hum}</td>`
+    }
+})
